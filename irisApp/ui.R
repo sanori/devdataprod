ecf16bf27843b30ff686d306fbc15bc3b5ceff7e
Joo-Won Jung <sanori@gmail.com>
2015-07-27 04:56:23 +0900

    Fix typo

--- irisApp/ui.R
+++ irisApp/ui.R
@@ -7,7 +7,7 @@ shinyUI(fluidPage(
       p("Select variable for X and Y axis"),
       selectInput("x", label = "X", choices = columns, selected = "Sepal.Length"),
       selectInput("y", label = "Y", choices = columns, selected = "Sepal.Width"),
-      checkboxInput("showCenter", label = "Show center of species", value = FALSE),
+      checkboxInput("showCenter", label = "Show centers of each species", value = FALSE),
       helpText("This app shows the data distribution of iris data set.
                As you choose the variable for x/y axis,
                it will show the graph with species of iris.")
